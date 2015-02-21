ce455bfb7737dd1ecfe4cff62560b32006b78bc1
Paul Bae <pbae@utexas.edu>
2015-02-21 10:12:58 -0600

    done with init-python

--- config/init-python.el
+++ config/init-python.el
@@ -1,15 +1,3 @@
-(require-package 'jedi)
-(require-package 'epc)
-(require-package 'elpy)
+;; not much to do here
 
-(add-hook 'python-mode-hook 'jedi:ac-setup)
-(setq jedi:setup-keys t)                      ; optional
-(setq jedi:complete-on-dot t)                 ; optional
-
-(setq elpy-default-minor-modes '(eldoc-mode
-                                 highlight-indentation-mode
-                                 yas-minor-mode))
-(setq elpy-rpc-backend "jedi")
-
-(elpy-enable)
 (provide 'init-python)
